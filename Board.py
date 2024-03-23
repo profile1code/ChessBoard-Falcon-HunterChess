444d62f51ab3131255031e6b561271ba516e8837
James Osborn <james-osborn@hotmail.com>
2024-03-23 00:41:35 -0700

    Fixed some bugs + Privated all class variables

--- Board.py
+++ Board.py
@@ -1,6 +1,3 @@
-# https://www.pngfind.com/mpng/TiximRR_chess-pieces-sprite-chess-pieces-sprite-sheet-hd/
-# https://stackoverflow.com/questions/66467383/how-to-draw-a-chessboard-with-pygame-and-move-the-pieces-on-the-board#:~:text=You%20have%20to%20create%20a%20pygame.sprite.Sprite%20object%20for,class%20ChessSprite%28pygame.sprite.Sprite%29%3A%20def%20__init__%28self%2C%20i%2C%20j%2C%20image%29%3A%20super%28%29.__init__%28%29
-
 # James Osborn
 # This class incorporates the ChessVar class into an interactable board to play on
 
@@ -8,112 +5,134 @@
 # Import the necessary libraries
 import pygame
 import ChessVar
+import os
+import time
 
 
-class DragOperator:
-    def __init__(self, sprite):
-        self.sprite = sprite
-        self.dragging = False
-        self.rel_pos = (0, 0)
+# Constants / Setup
+screen_width = 1200
+screen_height = int(screen_width * 9 / 16)
 
-    def update(self, event_list):
-        for event in event_list:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                self.dragging = self.sprite.rect.collidepoint(event.pos)
-                self.rel_pos = event.pos[0] - self.sprite.rect.x, event.pos[1] - self.sprite.rect.y
-            if event.type == pygame.MOUSEBUTTONUP:
-                self.dragging = False
-            if event.type == pygame.MOUSEMOTION and self.dragging:
-                self.sprite.rect.topleft = event.pos[0] - self.rel_pos[0], event.pos[1] - self.rel_pos[1]
+fps = 60
+clock = pygame.time.Clock()
 
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Falcon-Hunter Chess')
 
-class ChessSprite(pygame.sprite.Sprite):
-    def __init__(self, board_rect, i, j, image):
-        super().__init__()
-        self.board = board_rect
-        self.image = image
-        self.set_pos(i, j)
-        self.drag = DragOperator(self)
+black_square_color = (73, 97, 209)
+white_square_color = (240, 240, 200)
+falcon_hunter_rectangle_color = (150, 200, 255)
 
-    def set_pos(self, i, j):
-        x = self.board.left + self.board.width // 8 * i + self.board.width // 16
-        y = self.board.left + self.board.height // 8 * (7 - j) + self.board.height // 16
-        self.rect = self.image.get_rect(center=(x, y))
-
-    def update(self, event_list):
-        self.drag.update(event_list)
-        if not self.drag.dragging:
-            i = max(0, min(7, (self.rect.centerx - self.board.left) // (self.board.width // 8)))
-            j = 7 - max(0, min(7, (self.rect.centery - self.board.top) // (self.board.height // 8)))
-            self.set_pos(i, j)
+ratio = screen_width / 16
 
 
 def main():
-    pygame.display.init()
+    
+    # Creates the game object
+    pygame.init()
+    run = True
 
-    size = 60
-    white, black, red = (255, 220, 220), (70, 80, 255), (50, 50, 50)
+    chess_game = ChessVar.ChessVar()
+    draw_board(chess_game._board)
 
-    # Set up the display window
-    screen = pygame.display.set_mode((900, 600))
-    clock = pygame.time.Clock()
-    pygame.display.set_caption("Falcon-Hunter Chess Variant")
-    board = pygame.Surface(screen.get_size())
-    board.fill(white)
-
-
-
-    boardLength = 8
-    screen.fill(red)
-
-    IMAGES = {}
-    pieces = ['wp', 'wr', 'wn', 'wb', 'wk', 'wq', 'bp', 'br', 'bn', 'bb', 'bk', 'bq']
-    for piece in pieces:
-        IMAGES[piece] = pygame.transform.scale(
-                            pygame.image.load("ChessPieces/" + piece + ".png"),
-                                              (size, size))
-    white_pieces = {}
-    for i, f in enumerate(['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr']):
-        white_pieces[f] = ChessSprite(i, 0, IMAGES[f])
-    for i in range(8):
-        pawn_name = 'wp' + str(i+1)
-        white_pieces[pawn_name] = ChessSprite(i, 1, IMAGES['wp'])
-
-    black_pieces = {}
-    for i, f in enumerate(['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br']):
-        black_pieces[f] = ChessSprite(i, 7, IMAGES[f])
-    for i in range(8):
-        pawn_name = 'bp' + str(i+1)
-        black_pieces[pawn_name] = ChessSprite(i, 6, IMAGES['bp'])
-
-    group = pygame.sprite.Group()
-    group.add(white_pieces.values())
-    group.add(black_pieces.values())
-
-
-    count = 0
-    for row in range(1, boardLength+1):
-        for col in range(1, boardLength+1):
-            if count % 2 == 0:
-                pygame.draw.rect(screen, white, [size * col, size * row, size, size])
-            else:
-                pygame.draw.rect(screen, black, [size * col, size * row, size, size])
-            count += 1
-        count -= 1
-
-    pygame.display.update()
-
-    running = True
-    while running:
+    selected_piece = None
+    initial_square = None
+    
+    while run:
+        find_square_from_mouse()
+        # Takes a list of events
         for event in pygame.event.get():
+            # Checks whether the X in the top right is clicked
             if event.type == pygame.QUIT:
-                running = False
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN and find_square_from_mouse() != 'Off Board':
+                initial_square = find_square_from_mouse()
+                row, column = ChessVar.get_board_indexes(initial_square)
+                piece = chess_game._board[row][column]
+                if '[]' not in piece.get_code():
+                    selected_piece = piece
+                    piece_square = initial_square
+            if event.type == pygame.MOUSEBUTTONUP and selected_piece is not None:
+                final_square = find_square_from_mouse()
+                if final_square == 'Off Board':
+                    final_square = initial_square
+                move = chess_game.make_move(initial_square, final_square)
+                selected_piece = None
+                initial_square = None
 
-        # Update display
-        pygame.display.flip()
+        if chess_game.get_game_state() is not 'UNFINISHED':
+            player_wins(chess_game)
 
-    # Clean up
+        draw_board(chess_game._board, selected_piece)
+        pygame.display.update()
+        clock.tick(fps)
+
     pygame.quit()
+    
+
+def draw_board(board, selected_piece=None):
+    screen.fill((0, 0, 0))
+
+    # Draws the board
+    for row in range(8):
+        for column in range(8):
+            color = ''
+            if (row + column) % 2 == 0:
+                color = black_square_color
+            else:
+                color = white_square_color
+            square = pygame.Rect((ratio * (column + 1), screen_height - (ratio * (row + 1.5)), ratio, ratio))
+            pygame.draw.rect(screen, color, square)
+
+    rectangle = pygame.Rect((ratio * (8 + 1), screen_height - (ratio * (7 + 1.5)), ratio * 1.5, ratio * 8))
+    pygame.draw.rect(screen, falcon_hunter_rectangle_color, rectangle)
+
+    # Draws the pieces
+    for row in range(8):
+        for col in range(8):
+            piece = board[row][col]
+            color = 'w'
+            if piece.get_color() == 'Black':
+                color = 'b'
+            code = piece.get_code().lower()
+            if '[]' not in code:
+                dir = os.path.dirname(os.path.realpath(__file__))
+                location = dir.strip() + "\\ChessPieces\\".strip() + color.strip() + code.strip() + '.png'.strip()
+                img = pygame.image.load(location).convert_alpha()
+                img = pygame.transform.scale(img, (ratio, ratio))
+                # Checks if a piece is currently being dragged from its position
+                if selected_piece == piece and selected_piece is not None and len(find_square_from_mouse()) == 2:
+                    x, y = pygame.mouse.get_pos()
+                    x -= ratio / 2
+                    y -= ratio / 2 
+                    print('Selected')
+                else:
+                    x, y = get_square_location(row, col)
+                screen.blit(img, (x, y))
+
+
+def find_square_from_mouse():
+    """Returns what square the mouse is on, or if it's somewhere else"""
+    x, y = pygame.mouse.get_pos()
+    column = int(x / ratio) - 1
+    row = int((screen_height - y) / ratio - 0.5)
+    if ChessVar.is_on_board(row, column):
+        return ChessVar.get_board_notation(row, column)
+    elif 0 <= row <= 7 and 7 <= column <= 10.5:
+        print(row, column)
+        return 'Falcon/Hunter'
+    else:
+        return 'Off Board'
+    
+def get_square_location(row, column):
+    """Returns the pixel location of a square on the board"""
+    x = ratio * (column + 1)
+    y = screen_height - (ratio * (row + 1.5))
+    return (x, y)
+
+def player_wins(game):
+    screen.fill((0, 0, 0))
+
 
 if __name__ == '__main__':
     main()
